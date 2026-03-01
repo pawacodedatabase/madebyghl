aeba7cf167d242930c93cc01b74e9b4dcd7e1996
Euler-sys <euler@atomicmail.io>
2026-03-01 19:51:57 +0100

    pop

--- src/Pages/components/occasionedit.tsx
+++ src/Pages/components/occasionedit.tsx
@@ -8,7 +8,7 @@ const OccasionEdit: React.FC = () => {
         {/* Text Section */}
         <div className="w-full md:w-1/2 p-8 md:p-16 text-center md:text-left space-y-4">
           <h2 className="text-2xl md:text-3xl font-graffiti font-bold text-black">
-            <span className="text-red-500">Roc</span>space™
+            <span className="text-green-800">Made</span>ByGHL™
           </h2>
           <hr className="w-12 border-black mx-auto md:mx-0" />
           <p className="text-black text-base md:text-lg">Make a lasting impression.</p>
